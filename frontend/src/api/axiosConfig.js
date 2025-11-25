754d1c5475a1cf04c722a3282fdfb7df1281488a
James Jones <jjsfitnesstoronto@gmail.com>
2025-11-25 07:22:51 -0500

    Added backend/Dockerfile, frontend/Dockerfile, and docker-compose.yml

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // Update to Docker container later if needed
+  baseURL: "http://backend:5000/api", // For Docker!
 });
 
+/*
+const API = axios.create({
+  baseURL: "http://localhost:5000/api", // Update to Docker container later if needed
+});
+*/
+
+// Attach JWT automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
