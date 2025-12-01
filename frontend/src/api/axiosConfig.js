8c2379ac1ec0872fd276ce3583de842bfaa99590
James Jones <jjsfitnesstoronto@gmail.com>
2025-11-30 20:42:41 -0500

    Refactored [200~src/api/axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -49,7 +49,7 @@ const API = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:5000/api"
-      : "https://comp3123-backend5.onrender.com/api", // Add `/api` here
+      : "https://comp3123-backend5.onrender.com", // Add `/api` here
 });
 
 // Attach JWT automatically with safety
