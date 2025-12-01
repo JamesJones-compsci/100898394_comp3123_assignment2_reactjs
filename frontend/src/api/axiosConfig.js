f8f257adcc0848a7cf6e15d734363cd4ebbd3412
James Jones <jjsfitnesstoronto@gmail.com>
2025-11-30 19:48:32 -0500

    Fixed deployed backend baseURL to include /api

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -36,13 +36,21 @@ const API = axios.create({
 });
 */
 
+/*
 const API = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:5000/api"             // Local backend
       : "https://comp3123-backend5.onrender.com", // Deployed backend
 });
+*/
 
+const API = axios.create({
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:5000/api"
+      : "https://comp3123-backend5.onrender.com/api", // Add `/api` here
+});
 
 // Attach JWT automatically with safety
 API.interceptors.request.use(
